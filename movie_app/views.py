a9576e2dfa857f66462659b1a1999fe603acdb49
Azimbek001 <azimmustapaev@gmail.com>
2023-08-24 00:34:58 +0600

    hw3

--- movie_app/views.py
+++ movie_app/views.py
@@ -7,49 +7,95 @@ from django.shortcuts import get_object_or_404
 from rest_framework.generics import ListAPIView
 from rest_framework.pagination import PageNumberPagination
 
-@api_view(['GET'])
+
+@api_view(['GET', 'POST'])
 def director_list(request):
     if request.method == 'GET':
         directors = Director.objects.all()
-        serializer = DirectorSerializer(directors, many=True)
-        return Response(data=serializer.data)
+        data = DirectorSerializer(directors,many=True)
+        return Response(data.data)
+    elif request.method == 'POST':
+        serializer = DirectorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def get_director(request, id):
     director = get_object_or_404(Director, id=id)
-    serializer = DirectorSerializer(director, many=False)
-    return Response(data=serializer.data)
+    if request.method == 'GET':
+        serializer = DirectorSerializer(director)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = DirectorSerializer(director, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        director.delete()
+        return Response(status=200)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def movie_list(request):
     if request.method == 'GET':
         movies = Movie.objects.all()
-        serializer = MovieSerializer(movies, many=True)
-        return Response(data=serializer.data)
+        data = MovieSerializer(movies, many=True)
+        return Response(data.data)
+    elif request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def get_movie(request, id):
     movie = get_object_or_404(Movie, id=id)
-    serializer = MovieSerializer(movie, many=False)
-    return Response(data=serializer.data)
+    if request.method == 'GET':
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        movie.delete()
+        return Response(status=200)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def review_list(request):
     if request.method == 'GET':
-        movies = Review.objects.all()
-        serializer = ReviewSerializer(movies, many=True)
-        return Response(data=serializer.data)
+        review = Review.objects.all()
+        data = ReviewSerializer(review, many=True)
+        return Response(data.data)
+    elif request.method == 'POST':
+        serializer = ReviewSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def get_review(request, id):
     review = get_object_or_404(Review, id=id)
-    serializer = ReviewSerializer(review ,many=False)
-    return Response(data=serializer.data)
+    if request.method == 'GET':
+        serializer = ReviewSerializer(review)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ReviewSerializer(review, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        review.delete()
+        return Response(status=200)
 
 
 class MoviesReviewAPIView(ListAPIView):
